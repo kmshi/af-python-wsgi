a1206a4331e416975aecb5a5c2eb150a3e91d8b3
Richard Shi <kmshi_array@yahoo.com>
2012-12-05 23:33:31 +0800

    First checkin

---
+++ console.py
@@ -0,0 +1,9 @@
+'''
+Created on 2012-12-5
+
+@author: apple
+'''
+from bottle import run
+import wsgi
+
+run(host='localhost', port=8080)
--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,30 @@
-import bottle
-import os
+#import bottle
+#import os
 
-def application(environ, start_response):
-    data = "Hello World! AppFog Python Support"
-    start_response("200 OK", [
-            ("Content-Type", "text/plain"),
-            ("Content-Length", str(len(data)))
-            ])
-    return iter([data])
+#def application(environ, start_response):
+#    data = "Hello World! AppFog Python Support"
+#    start_response("200 OK", [
+#            ("Content-Type", "text/plain"),
+#            ("Content-Length", str(len(data)))
+#            ])
+#    return iter([data])
+
+from bottle import route, run, debug, template, request, validate, static_file, error
+
+# only needed when you run Bottle on mod_wsgi
+from bottle import default_app
+
+@route('/hello/:name')
+def index(name='World'):
+    return template('<b>Hello {{name}}</b>!', name=name)
+
+@error(403)
+def mistake403(code):
+    return 'There is a mistake in your url!'
+
+@error(404)
+def mistake404(code):
+    return 'Sorry, this page does not exist!'
+
+application = default_app()
 
